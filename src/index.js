55a5b165e2a96151116b8a2d16398cdfa9fd0aec
Renaat De Muynck <renaat.demuynck@arteveldehs.be>
2020-02-24 09:55:48 +0100

    Add functions for getting text paths for the sections

--- src/index.js
+++ src/index.js
@@ -157,3 +157,107 @@ export function getWheelSegmentPaths(segmentCount, outerRadius, innerRadius = 0,
         return getWheelSegmentPath(startAngle, endAngle, outerRadius, innerRadius, spokeWidth, origin);
     });
 }
+
+/**
+ *
+ * @param {number} startAngle - The start angle of the segment
+ * @param {number} endAngle - The end angle of the segment
+ * @param {number} outerRadius - The outer radius of the segment
+ * @param {number} innerRadius - The inner radius of the segment
+ * @param {number} spokeWidth - The width of the space between the segments
+ * @param {Point} origin - The origin point of the inner and outer circles that make up the segments
+ */
+export function getWheelSegmentArcTextPath(startAngle, endAngle, outerRadius, innerRadius = 0, spokeWidth = 0, origin = { x: 0, y: 0 }) {
+    let leftLine = getRightParallelLine(
+        origin,
+        getPointOnCircle(startAngle, outerRadius, origin),
+        spokeWidth / 2
+    );
+    let rightLine = getLeftParallelLine(
+        origin,
+        getPointOnCircle(endAngle, outerRadius, origin),
+        spokeWidth / 2
+    );
+
+    let middleRadius = (innerRadius + outerRadius) / 2;
+
+    let leftLineShape = ShapeInfo.line(leftLine.start, leftLine.end);
+    let rightLineShape = ShapeInfo.line(rightLine.start, rightLine.end);
+    let circleShape = ShapeInfo.circle(origin, middleRadius);
+
+    let textPathStart = Intersection.intersect(leftLineShape, circleShape).points[0];
+    let textPathEnd = Intersection.intersect(rightLineShape, circleShape).points[0];
+
+    return `
+        M ${textPathStart.x} ${textPathStart.y}
+        A ${middleRadius} ${middleRadius} 0 0 1 ${textPathEnd.x} ${textPathEnd.y}
+    `;
+}
+
+/**
+ *
+ * @param {number} segmentCount - The number of segments to divide the wheel in
+ * @param {number} outerRadius - The outer radius of the wheel
+ * @param {number} innerRadius - The inner radius of the wheel
+ * @param {number} spokeWidth - The width of the space between the segments
+ * @param {number} angleOffset - The starting angle of the first segment (starting from the top)
+ * @param {Point} origin - The origin point of the inner and outer circles that make up the segments
+ */
+export function getWheelSegmentArcTextPaths(segmentCount, outerRadius, innerRadius = 0, spokeWidth = 0, angleOffset = 0, origin = { x: 0, y: 0 }) {
+    let segmentLength = 360 / segmentCount;
+
+    return Array.from({ length: segmentCount }, (currentValue, index) => {
+        let startAngle = angleOffset + (index * segmentLength);
+        let endAngle = startAngle + segmentLength;
+
+        return getWheelSegmentArcTextPath(startAngle, endAngle, outerRadius, innerRadius, spokeWidth, origin);
+    });
+}
+
+/**
+ *
+ * @param {number} startAngle - The start angle of the segment
+ * @param {number} endAngle - The end angle of the segment
+ * @param {number} outerRadius - The outer radius of the segment
+ * @param {number} innerRadius - The inner radius of the segment
+ * @param {number} spokeWidth - The width of the space between the segments
+ * @param {Point} origin - The origin point of the inner and outer circles that make up the segments
+ */
+export function getWheelSegmentLineTextPath(startAngle, endAngle, outerRadius, innerRadius = 0, spokeWidth = 0, origin = { x: 0, y: 0 }) {
+    let middleAngle = (startAngle + endAngle) / 2;
+
+    let lineStart = getPointOnCircle(middleAngle, innerRadius, origin);
+    let lineEnd = getPointOnCircle(middleAngle, outerRadius + 1, origin);
+
+    let lineShape = ShapeInfo.line(lineStart, lineEnd);
+    let innerCircleShape = ShapeInfo.circle(origin, innerRadius);
+    let outerCircleShape = ShapeInfo.circle(origin, outerRadius);
+
+    let textPathStart = Intersection.intersect(lineShape, innerCircleShape).points[0];
+    let textPathEnd = Intersection.intersect(lineShape, outerCircleShape).points[0];
+
+    return `
+        M ${textPathStart.x} ${textPathStart.y}
+        L ${textPathEnd.x} ${textPathEnd.y}
+    `;
+}
+
+/**
+ *
+ * @param {number} segmentCount - The number of segments to divide the wheel in
+ * @param {number} outerRadius - The outer radius of the wheel
+ * @param {number} innerRadius - The inner radius of the wheel
+ * @param {number} spokeWidth - The width of the space between the segments
+ * @param {number} angleOffset - The starting angle of the first segment (starting from the top)
+ * @param {Point} origin - The origin point of the inner and outer circles that make up the segments
+ */
+export function getWheelSegmentLineTextPaths(segmentCount, outerRadius, innerRadius = 0, spokeWidth = 0, angleOffset = 0, origin = { x: 0, y: 0 }) {
+    let segmentLength = 360 / segmentCount;
+
+    return Array.from({ length: segmentCount }, (currentValue, index) => {
+        let startAngle = angleOffset + (index * segmentLength);
+        let endAngle = startAngle + segmentLength;
+
+        return getWheelSegmentLineTextPath(startAngle, endAngle, outerRadius, innerRadius, spokeWidth, origin);
+    });
+}
